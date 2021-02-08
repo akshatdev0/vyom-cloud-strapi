a596d12c6a077b800a337f6347d8b99c1d7b4c59
Akshat Nigam <akshat.dev.zero@gmail.com>
2021-02-09 01:45:54 +0530

    proxy value set to true in config/server.js

--- config/server.js
+++ config/server.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: env('PUBLIC_URL', 'https://api.exilien.cloud'),
+  proxy: env.bool('IS_PROXIED', true),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '526100d0d704f048b111bda012e12cf2'),
