62413e7b82ff03af931909c9ce31e18d350b0230
Akshat Nigam <akshat.dev.zero@gmail.com>
2021-02-09 00:54:32 +0530

    PUBLIC_URL added in config/server.js

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://api.exilien.cloud'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '526100d0d704f048b111bda012e12cf2'),
