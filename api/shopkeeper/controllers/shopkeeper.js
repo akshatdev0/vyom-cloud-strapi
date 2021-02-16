a66b420be3c597e1cb50146ff88aca1ad6a4cdf4
Akshat Nigam <akshat.dev.zero@gmail.com>
2021-02-17 02:02:42 +0530

    Shopkeeper.create controller action overidden.

--- api/shopkeeper/controllers/shopkeeper.js
+++ api/shopkeeper/controllers/shopkeeper.js
@@ -1,8 +1,101 @@
 'use strict';
 
 /**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
- * to customize this controller
+ * shopkeeper.js controller
+ *
+ * @description: A set of functions called "actions" for managing `Shopkeeper`.
  */
 
-module.exports = {};
+ /* eslint-disable no-useless-escape */
+const _ = require("lodash");
+const { sanitizeEntity } = require('strapi-utils');
+
+const emailRegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const formatError = (error) => [
+  { messages: [{ id: error.id, message: error.message, field: error.field }] },
+];
+
+const USER_PROPERTIES = [
+    'firstName',
+    'lastName',
+    'gender',
+    'dateOfBirth',
+    'email',
+    'alternateMobileNumber',
+    'blocked', 
+];
+
+module.exports = {
+
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
+  async create(ctx) {
+    const ctxUser = ctx.state.user;
+
+    if (!ctxUser) {
+      return ctx.badRequest(
+        null,
+        formatError({
+          id: "shopkeeper.create.error.auth.header.missing",
+          message: "No authorization header was found.",
+        })
+      );
+    }
+
+    const role = await strapi
+      .query('role', 'users-permissions')
+      .findOne({ type: 'shopkeeper' }, []);
+
+    if (!role) {
+      return ctx.badRequest(
+        null,
+        formatError({
+          id: 'shopkeeper.create.error.role.notFound',
+          message: "Not able to find the 'Shopkeeper' role.",
+        })
+      );
+    }
+
+    const userValues = _.pick(ctx.request.body, USER_PROPERTIES);
+    
+    if (userValues.email) {
+      if (emailRegExp.test(userValues.email)) {
+        userValues.email = userValues.email.toLowerCase();
+      } else {
+        return ctx.badRequest(
+            null,
+            formatError({
+            id: 'shopkeeper.create.error.email.format',
+            message: 'Please provide valid email address.',
+            })
+        );
+      }
+    }
+
+    if (userValues.alternateMobileNumber) {
+      const phoneNumber = phoneNumberUtil.parseAndKeepRawInput(
+        userValues.alternateMobileNumber,
+        "IN"
+      );
+      if (
+        !phoneNumberUtil.isValidNumberForRegion(phoneNumber, "IN") ||
+        phoneNumberUtil.getNumberType(phoneNumber) !== PhoneNumberType.MOBILE
+      ) {
+        return ctx.badRequest(
+          null,
+          formatError({
+            id: "shopkeeper.create.error.alternateMobileNumber.invalid",
+            message: "Please provide a valid alternate mobile number.",
+          })
+        );
+      }
+    }
+
+    await strapi.query('user', 'users-permissions').update({ id: ctxUser.id }, { role: role.id, ...userValues });
+    const entity = await strapi.services.shopkeeper.create({ user: ctxUser.id });
+    return sanitizeEntity(entity.toJSON ? entity.toJSON() : entity, { model: strapi.models.shopkeeper });
+  }
+};
