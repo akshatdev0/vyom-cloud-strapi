ed655c9eea20f6a300a8dae7cf160d8014e39dc6
Akshat Nigam <akshat.dev.zero@gmail.com>
2021-02-16 16:58:15 +0530

    Auth.register overridden to accept only mobileNumber and create a user.

---
+++ extensions/users-permissions/controllers/Auth.js
@@ -0,0 +1,94 @@
+"use strict";
+
+/**
+ * Auth.js controller extension
+ *
+ * @description: A set of functions called "actions" for managing `Auth`.
+ */
+
+/* eslint-disable no-useless-escape */
+const crypto = require("crypto");
+const _ = require("lodash");
+const grant = require("grant-koa");
+const { sanitizeEntity } = require("strapi-utils");
+
+module.exports = {
+  async register(ctx) {
+    const pluginStore = await strapi.store({
+      environment: "",
+      type: "plugin",
+      name: "users-permissions",
+    });
+
+    const settings = await pluginStore.get({
+      key: "advanced",
+    });
+
+    if (!settings.allow_register) {
+      return ctx.badRequest(
+        null,
+        formatError({
+          id: "Auth.advanced.allow_register",
+          message: "Register action is currently disabled.",
+        })
+      );
+    }
+
+    const params = {
+      ..._.omit(ctx.request.body, [
+        "confirmed",
+        "confirmationToken",
+        "resetPasswordToken",
+      ]),
+      provider: "local",
+    };
+
+    // Mobile Number (username) is required.
+    if (!params.username) {
+      return ctx.badRequest(
+        null,
+        formatError({
+          id: "Auth.form.error.mobileNumber.provide",
+          message: "Please provide your mobile number.",
+        })
+      );
+    }
+
+    const user = await strapi.query("user", "users-permissions").findOne({
+      username: params.username,
+    });
+
+    if (user) {
+      return ctx.badRequest(
+        null,
+        formatError({
+          id: "Auth.form.error.mobileNumber.taken",
+          message: "Mobile number is already taken.",
+        })
+      );
+    }
+
+    try {
+      const user = await strapi
+        .query("user", "users-permissions")
+        .create(params);
+
+      const sanitizedUser = sanitizeEntity(user, {
+        model: strapi.query("user", "users-permissions").model,
+      });
+
+      return ctx.send({
+        user: sanitizedUser,
+      });
+    } catch (err) {
+      const adminError = _.includes(err.message, "username")
+        ? {
+            id: "Auth.form.error.mobileNumber.taken",
+            message: "Mobile number is already taken.",
+          }
+        : { id: "Auth.form.error.email.taken", message: "Email already taken" };
+
+      ctx.badRequest(null, formatError(adminError));
+    }
+  },
+};
