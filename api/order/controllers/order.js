e2a473653a109b33b6f9f115f5648132fd453fed
Akshat Nigam <akshat.dev.zero@gmail.com>
2021-03-05 15:39:31 +0530

    _createOrder graphql schema defined.

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -5,4 +5,29 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  /**
+   * Create a new order with order lines.
+   *
+   * @return {Object}
+   */
+  async _create(ctx) {
+    // Todo
+    // * Put checks
+    //   - To restrict the action for shopkeeper's own shop
+    //   - To restrict the action for salesman
+    //     - to be within this shop's range
+    //     - to have been verified by this shop's shopkeeper using OTP
+
+    console.log("params");
+    console.log("-----------------");
+    console.log(JSON.stringify(ctx.params));
+    console.log("xxxxxxxxxxxxxxxxx");
+    console.log("body");
+    console.log("-----------------");
+    console.log(JSON.stringify(ctx.request.body));
+    console.log("xxxxxxxxxxxxxxxxx");
+
+    ctx.send({ message: "Hello Order" });
+  },
+};
