74ad19d30f904fed01fa87bd86f64513e2f4c71d
Akshat Nigam <akshat.dev.zero@gmail.com>
2021-08-13 21:28:08 +0530

    Added countOrders GraphQL Resolver.

--- api/product/config/schema.graphql.js
+++ api/product/config/schema.graphql.js
@@ -1,17 +1,18 @@
 module.exports = {
-    query: `
+  query: `
       countProducts(where: JSON): Int!
-    `,
-    resolver: {
-      Query: {
-        countProducts: {
-          description: 'Return the count of products',
-          resolverOf: 'application::product.product.count',
-          resolver: async (obj, options, ctx) => {
-            return await strapi.api.product.services.product.count(options.where || {});
-          },
+  `,
+  resolver: {
+    Query: {
+      countProducts: {
+        description: "Return the count of products",
+        resolverOf: "application::product.product.count",
+        resolver: async (_obj, options) => {
+          return await strapi.api.product.services.product.count(
+            options.where || {}
+          );
         },
       },
     },
-  };
-            
+  },
+};
