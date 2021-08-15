0ba6e6c6a9750293ed63a446dc1904c37bab24d1
Akshat Nigam <akshat.dev.zero@gmail.com>
2021-08-15 15:44:01 +0530

    ProductCategory type extended to add hasSubCategory field.

---
+++ api/product-category/config/schema.graphql.js
@@ -0,0 +1,7 @@
+module.exports = {
+  definition: `
+    extend type ProductCategory {
+      hasSubCategories: Boolean
+    }
+  `,
+};
--- api/product-category/controllers/product-category.js
+++ api/product-category/controllers/product-category.js
@@ -4,5 +4,41 @@
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
+const { sanitizeEntity } = require("strapi-utils");
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let e1;
+    if (ctx.query._q) {
+      e1 = await strapi.services["product-category"].search(ctx.query);
+    } else {
+      e1 = await strapi.services["product-category"].find(ctx.query);
+    }
+
+    const e2 = [];
+    for (let i = 0; i < e1.length; i++) {
+      const count = await strapi.services["product-category"].count({
+        parentCategory: e1[i].id,
+      });
+      e2.push({
+        ...e1[i],
+        hasSubCategories: count > 0,
+      });
+    }
+
+    return e2.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models["product-category"] })
+    );
+  },
+
+  async findOne(ctx) {
+    const { id } = ctx.params;
+    console.log("one");
+    const entity = await strapi.services["product-category"].findOne({ id });
+    const count = await strapi.services["product-category"].count({
+      parentCategory: entity.id,
+    });
+    entity.hasSubCategories = count > 0;
+    return sanitizeEntity(entity, { model: strapi.models["product-category"] });
+  },
+};
